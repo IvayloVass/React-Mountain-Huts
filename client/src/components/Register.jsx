7070ef8589337f92c3588260b1bfc65023e83d40
IvayloVass <ivaylovasileff@gmail.com>
2023-12-09 00:23:43 +0200

    Fixed typo

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -34,7 +34,7 @@ const Register = () => {
         let registerAuth = await register(regData);
         setAuth(registerAuth);
         sessionStorage.setItem('accessToken', registerAuth.accessToken);
-        sessionStorage.setItem('userId', respAuth._id);
+        sessionStorage.setItem('userId', registerAuth._id);
         navigate('/');
     }
 
